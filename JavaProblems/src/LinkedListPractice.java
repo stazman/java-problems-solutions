c2bd14f74d1aaed71cc253d8d728fc3df8567b95
Christopher DiStasio <cadistasio@gmail.com>
2020-07-27 16:05:11 -0500

    Adds further practice

--- JavaProblems/src/LinkedListPractice.java
+++ JavaProblems/src/LinkedListPractice.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class LinkedListPractice {
 	
@@ -9,27 +12,43 @@ public class LinkedListPractice {
 	public static void main(String[] args) {
 		
 		
-		List<String> l1 = new LinkedList<>();
+		int[] i = new int[10];
 		
-	     
+		int len = i.length;
+		
+		List<String> l3 = new ArrayList<>();
+		
+		 l3.add("Zara");
+	     l3.add("Mahnaz");
+	     l3.add("Ayan");
+	     l3.add("Zara");
+		
+		Set<String> l1 = new HashSet<>();
+		
+		Set<String> l2 = new HashSet<>();
 		
 		 l1.add("Zara");
 	     l1.add("Mahnaz");
 	     l1.add("Ayan");
-	     System.out.println();
-	     System.out.println(" LinkedList Elements");
-	    
-	     System.out.print("\t" + l1.get(2));
+	     l1.add("Zara");
 	     
-	     try {
-	    	 
-	    	 l1.get(5);
-			 
-		} catch (Exception e) {
-			
-			System.out.println("Not enough elements in list");
-			
-		}
+//	     l2.add("Susan");
+	     
+	     System.out.println(l2.hashCode());
+	     System.out.println(l2);
+	     System.out.println(len);
+	     System.out.println(l1);
+	     System.out.println(l3);
+	     
+//	     try {
+//	    	 
+//	    	 l1.get(5);
+//			 
+//		} catch (Exception e) {
+//			
+//			System.out.println("Not enough elements in list");
+//			
+//		}
 		
 	}
 	
